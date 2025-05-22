21616c4d7c2d4cd42cb92ef474f20d01756e3032
Kim Ruzzel Leron Enteria <kimenteria92@gmail.com>
2025-05-22 18:41:19 +0800

    update Kinala,tsx

--- app/(tabs)/recipes/Kinalas.tsx
+++ app/(tabs)/recipes/Kinalas.tsx
@@ -35,7 +35,7 @@ const SiliIceCream = () => {
 
         {/* Picture under the title */}
         <Image
-          source={require("../../../assets/images/Sili-Ice-Cream.jpg")} // Correct way to load local images
+          source={require("../../../assets/images/kinalas.png")} // Correct way to load local images
           style={styles.recipeImage}
         />
 
